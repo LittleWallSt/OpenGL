42a90b2687e6d8e19d9a44b341f7e892d885aac1
Javier Murillo González <96345143+LittleWallSt@users.noreply.github.com>
2024-10-24 16:51:53 +0200

    No triangle :d

--- OpenGL/src/app.cpp
+++ OpenGL/src/app.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -8,6 +10,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "OPENGLTEST", NULL, NULL);
     if (!window)
@@ -19,6 +22,26 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    //Initialize Glew, Library that gets functions from the GPU drivers
+    if (glewInit() != GLEW_OK)
+        std::cout << "error" << std::endl;
+
+    //Check the version of OPENGL
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    
+    // >> DATA FOR TRIANGLE
+    float positions[6] = {
+        -0.5f, -0.5,
+        0,  0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+    // << DATA FOR TRIANGLE
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -26,11 +49,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         
-        glBegin(GL_TRIANGLES);
-        glVertex2d(-0.5f, -0.5f);
-        glVertex2d(0, 0.5f);
-        glVertex2d(0.5f, -0.5f);
-        glEnd();
+        //Draw call, is blank because we don't have a shader implementation yet
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
