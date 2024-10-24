72e9dcc6071ae0210259b5b58d0ca1bfba8c8bfb
Javier Murillo González <96345143+LittleWallSt@users.noreply.github.com>
2024-10-24 18:01:24 +0200

    TRIANGLE IS RED

--- OpenGL/src/app.cpp
+++ OpenGL/src/app.cpp
@@ -2,6 +2,50 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader( unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result; 
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result); 
+    if (result == GL_FALSE)
+    {
+        int length; 
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)malloc(length * sizeof(char)); 
+        glGetShaderInfoLog(id, length, &length, message); 
+
+        std::cout << "Failed to compile " <<
+            (type == GL_FRAGMENT_SHADER ? "fragment " : "vertex ") << "shader" << std::endl; 
+        std::cout << message << std::endl; 
+        
+        glDeleteShader(id); 
+        return 0; 
+    }
+
+
+    //TODO: ERROR HANDLING 
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -29,6 +73,30 @@ int main(void)
     //Check the version of OPENGL
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    // >> SHADER
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+    
+    std::string fragmentShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader); 
+    // << SHADER
+
     
     // >> DATA FOR TRIANGLE
     // vertex POSITIONS, Vertexes have a lot more data in them(Color, position, normals etc..)
@@ -54,6 +122,8 @@ int main(void)
     // Sixth parameter is offset of change of what information of the vertex is being accessed
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float),0);
     // << DATA FOR TRIANGLE
+
+    glUseProgram(shader); 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -70,6 +140,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader); 
 
     glfwTerminate();
     return 0;
